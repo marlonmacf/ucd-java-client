9fe72a6f5cc4a3156c96abc8fa875f1cb0657e59
Marlon Andrel <iago.olvr@gmail.com>
2016-11-21 03:09:50 -0200

    Updated the CardioController.java

--- src/main/java/domain/controllers/CardioController.java
+++ src/main/java/domain/controllers/CardioController.java
@@ -3,12 +3,13 @@ package domain.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CardioController {
 
     @RequestMapping(value = "/cardio", method = RequestMethod.GET)
-    String index() {
+    String index(@RequestParam(value = "app", defaultValue = "app") String app) {
         return "cardio/index";
     }
 }
