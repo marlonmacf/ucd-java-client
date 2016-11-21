a914c2c4d612ca568cb981187205096a1af9e59a
Marlon Andrel <iago.olvr@gmail.com>
2016-11-21 03:11:51 -0200

    Updated the MainController.java

--- src/main/java/domain/controllers/MainController.java
+++ src/main/java/domain/controllers/MainController.java
@@ -1,14 +1,20 @@
 package domain.controllers;
 
+import domain.entities.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
 
+    static User loggedUser;
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    String index() {
+    String index(Model model) {
+
+        model.addAttribute("loggedUser", loggedUser);
         return "main/index";
     }
 }
